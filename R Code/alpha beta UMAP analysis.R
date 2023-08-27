480a04e1f7546aeb21e7a0589eba194db6e6a423
Andreas Felber <7538911+Brownbadger@users.noreply.github.com>
2023-08-27 13:41:45 -0700

    Updated code for clairty and editing nonfunctional areas as well as code the overwrites pre-existing variables.
    Added seeding for permanova tests.
    
    Updated PCoA plots to match UniFrac plot.

--- R Code/alpha beta UMAP analysis.R
+++ R Code/alpha beta UMAP analysis.R
@@ -3,60 +3,88 @@ library(phyloseq)
 library(ape)
 library(tidyverse)
 library(vegan)
-library(phyloseq)
-
 
 #### Load in RData ####
 load("rare_fish.RData")
-load("phyloseq_fish.RData")
 
 #### Alpha diversity - sampling site ######
 plot_richness(fish_rare) 
 
 plot_richness(fish_rare, measures = c("Shannon","Chao1")) 
 
-gg_richness_sampling <- plot_richness(fish_rare, x = "SAMPLE_TYPE", measures = c("Shannon","Chao1")) +
+gg_richness_sampling <- plot_richness(fish_rare, x = "sample_type", measures = c("Shannon","Chao1")) +
   xlab("Body sampling locations") +
-  geom_boxplot()
+  geom_boxplot() +
+  theme_classic()
 gg_richness_sampling
-
 
 ggsave(filename = "sampling_site_alpha.png"
        , gg_richness_sampling
        , height=4, width=6)
 
+#### Alpha diversity - substrata ######
+plot_richness(fish_rare) 
+
+plot_richness(fish_rare, measures = c("Shannon","Chao1")) 
+
+gg_richness_substrata <- plot_richness(fish_rare, x = "substrata_collection", measures = c("Shannon","Chao1")) +
+  xlab("Substrata") +
+  geom_boxplot() +
+  theme_classic()
+gg_richness_substrata
+
+ggsave(filename = "substrata_alpha.png"
+       , gg_richness_substrata
+       , height=4, width=6)
+
 estimate_richness(fish_rare)
+
 
 ### Evaluating significance of alpha diversity - sampling site ###
 #### Statistical analysis using PERMANOVA
 samp_dat_wdiv <- data.frame(sample_data(fish_rare), estimate_richness(fish_rare))
 dm_chao <- vegdist(t(otu_table(fish_rare)), method="chao") 
-adonis2(dm_chao ~ substrata_collection*SAMPLE_TYPE, data=samp_dat_wdiv)
+set.seed(0)
+adonis2(dm_chao ~ substrata_collection*sample_type, data=samp_dat_wdiv)
 ?vegdist
 
 #### Comparison of >2 means with ANOVA (Parametric) by sample site####
 alphadiv <- estimate_richness(fish_rare)
 samp_dat <- sample_data(fish_rare)
-samp_dat_wdiv <- data.frame(samp_dat, alphadiv)
+samp_dat_adiv <- data.frame(samp_dat, alphadiv)
 
 # Set up our linear model 
-lm_shannon_vs_SAMPLE <- lm(Shannon ~ SAMPLE_TYPE, dat=samp_dat_wdiv)
+lm_shannon_vs_sample <- lm(Shannon ~ sample_type, dat=samp_dat_adiv)
 # Calculate AOV
-anova_shannon_vs_SAMPLE <- aov(lm_shannon_vs_SAMPLE)
+anova_shannon_vs_sample <- aov(lm_shannon_vs_sample)
 # Summarize
-summary(anova_shannon_vs_SAMPLE)
+summary(anova_shannon_vs_sample)
 
-TukeyHSD(anova_shannon_vs_SAMPLE)
+TukeyHSD(anova_shannon_vs_sample)
+
+### Evaluating significance of alpha diversity - substrata ###
+#### Comparison of >2 means with ANOVA (Parametric) ####
+
+# Set up our linear model 
+lm_shannon_vs_substrata <- lm(log(Shannon) ~ substrata_collection, dat=samp_dat_adiv)
+# Calculate AOV
+anova_shannon_vs_substrata <- aov(lm_shannon_vs_substrata)
+# Summarize
+summary(anova_shannon_vs_substrata)
+
+TukeyHSD(anova_shannon_vs_substrata)
+
 
 #### Beta diversity - sampling site#####
 bc_dm <- distance(fish_rare, method="Weighted Unifrac")
 
 pcoa_bc <- ordinate(fish_rare, method="PCoA", distance=bc_dm)
 
-plot_ordination(fish_rare, pcoa_bc, color="SAMPLE_TYPE")
+plot_ordination(fish_rare, pcoa_bc, color="sample_type")
 
-gg_pcoa <- plot_ordination(fish_rare,pcoa_bc, color = "SAMPLE_TYPE") +
-  labs( col = "body sampling site")
+gg_pcoa <- plot_ordination(fish_rare,pcoa_bc, color = "sample_type") +
+  labs( col = "Body Sampling Site") +
+  theme_classic()
 gg_pcoa
 
 ggsave("sampling_site_beta_plot_pcoa.png"
@@ -71,7 +99,8 @@ pcoa_bc_substrata <- ordinate(fish_rare, method="PCoA", distance=bc_dm_substrata
 plot_ordination(fish_rare, pcoa_bc_substrata, shape ="substrata_collection")
 
 gg_pcoa_sub <- plot_ordination(fish_rare,pcoa_bc, shape="substrata_collection") +
-  labs(pch="substrata")
+  labs(pch="Substrata") +
+  theme_classic()
 gg_pcoa_sub
 
 ggsave("substrata_beta_plot_pcoa.png"
@@ -83,10 +112,11 @@ bc_dm_com <- distance(fish_rare, method="Weighted Unifrac")
 
 pcoa_bc_com <- ordinate(fish_rare, method="PCoA", distance=bc_dm_com)
 
-plot_ordination(fish_rare, pcoa_bc_com, color="SAMPLE_TYPE", shape ="substrata_collection")
+plot_ordination(fish_rare, pcoa_bc_com, color="sample_type", shape ="substrata_collection")
 
-gg_pcoa_com <- plot_ordination(fish_rare,pcoa_bc, color = "SAMPLE_TYPE", shape="substrata_collection") +
-  labs(pch="substrata", col = "body sampling site")
+gg_pcoa_com <- plot_ordination(fish_rare,pcoa_bc, color = "sample_type", shape="substrata_collection") +
+  labs(pch="Substrata", col = "Body Sampling Site") +
+  theme_classic()
 gg_pcoa_com
 
 ggsave("combined_beta_plot_pcoa.png"
@@ -95,40 +125,9 @@ ggsave("combined_beta_plot_pcoa.png"
 
 #### UMAP - sampling site##### -- ISSUES with codes 
 # load your omic data here as mydata
-library(M3C)
-r <- M3C(fish_rare,method=2)
-umap(fish_rare,labels=as.factor(r$SAMPLE_TYPE),printres = TRUE,printwidth = 24)
-
-umap(pollen$data,colvec=c('skyblue'))
-
-#### Alpha diversity - substrata ######
-plot_richness(fish_rare) 
-
-plot_richness(fish_rare, measures = c("Shannon","Chao1")) 
-
-gg_richness_substrata <- plot_richness(fish_rare, x = "substrata_collection", measures = c("Shannon","Chao1")) +
-  xlab("Substrata") +
-  geom_boxplot()
-gg_richness_substrata
-
-ggsave(filename = "substrata_alpha.png"
-       , gg_richness_substrata
-       , height=4, width=6)
-
-estimate_richness(fish_rare)
-
-### Evaluating significance of alpha diversity - substrata ###
-#### Comparison of >2 means with ANOVA (Parametric) ####
-alphadiv <- estimate_richness(fish_rare)
-samp_dat <- sample_data(fish_rare)
-samp_dat_wdiv <- data.frame(samp_dat, alphadiv)
-
-# Set up our linear model 
-lm_shannon_vs_substrata <- lm(log(Shannon) ~ substrata_collection, dat=samp_dat_wdiv)
-# Calculate AOV
-anova_shannon_vs_substrata <- aov(lm_shannon_vs_substrata)
-# Summarize
-summary(anova_shannon_vs_substrata)
-
-TukeyHSD(anova_shannon_vs_SAMPLE)
+# library(M3C)
+# r <- M3C(fish_rare,method=2)
+# umap(fish_rare,labels=as.factor(r$SAMPLE_TYPE),printres = TRUE,printwidth = 24)
+# 
+# umap(pollen$data,colvec=c('skyblue'))
 
